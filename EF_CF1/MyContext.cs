b114afe634103f364e1020f5381eb1e4be54aa55
Ayşenur Kara <aysenurk.95@outlook.com>
2018-12-27 13:44:15 +0300

    code first ile veritabanı oluşturuldu

---
+++ EF_CF1/MyContext.cs
@@ -0,0 +1,20 @@
+﻿using EF_CF1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_CF1
+{
+    public class MyContext : DbContext
+    {
+        public MyContext() : base("name=MyCon")
+        {
+
+        }
+        public virtual DbSet<Kategori> Kategoriler { get; set; }
+        public virtual DbSet<Urun> Urunler { get; set; }
+    }
+}
